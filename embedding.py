75d6446bcd77aa75d4cda411c8ba4a33939126db
Mark Hartnady <mark.hartnady@vrpconsulting.com>
2023-02-27 17:50:40 +0000

    Updates

--- embedding.py
+++ embedding.py
@@ -1,6 +1,7 @@
 import openai, json, os
 import numpy as np
 openai.api_key = os.environ["OPEN_AI_KEY"]
+#ambiguous sentences = I saw her duck; The chicken is ready to eat
 
 def gpt3_embedding(content, engine='text-similarity-ada-001'):
     content = content.encode(encoding='ASCII',errors='ignore').decode()
@@ -16,14 +17,16 @@ if __name__ == '__main__':
     your_word = input('Enter a life form: ')
     your_embedded_word = gpt3_embedding(your_word)
     
-    print(your_embedded_word)
-    exit()
+    #print(your_embedded_word)
+    #exit()
 
-    categories = ['plant', 'reptiles', 'mammals', 'fish', 'primates']
+    categories = ['plant', 'reptiles', 'mammals', 'fish', 'primates', 'birds']
     scores = list()
     for category in categories:
         scores.append({'category':category, 'score':similarity(your_embedded_word,gpt3_embedding('type of ' + category))})
 
-    scores = sorted(list_input, key=lambda x: x['score'], reverse=True) 
+    scores = sorted(scores, key=lambda x: x['score'], reverse=True)  
 
-    print(json.dumps(scores, indent=4))
+    print(json.dumps(scores, indent=4) + '\n')
+    
+    print(f"\"{your_word.upper()}\" typically falls under the category of \"{scores[0]['category'].upper()}\"")
--- tokenizer.py
+++ tokenizer.py
@@ -1,10 +1,15 @@
-import transformers
+import transformers, tiktoken
 
-def tokenize(prompt): 
+def tokenize_words(prompt):  
     tokenizer = transformers.AutoTokenizer.from_pretrained("openai-gpt")
     tokens = tokenizer.tokenize(prompt)
-    return tokens
+    return str(tokens)
+    
+def tokenize_semantic(string, encoding_name='cl100k_base'): 
+    encoding = tiktoken.get_encoding(encoding_name)     
+    return str(encoding.encode(string))
     
 string = input('Enter a term to tokenize: ')
 
-print(tokenize(string))
+print('Word Tokens: ' + tokenize_words(string))
+print('Numeric Tokens: ' + tokenize_semantic(string))
